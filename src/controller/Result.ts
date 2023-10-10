9608699da7d73e256730e00d63edb7fb69ec0fa3
David <dgmurgulet@gmail.com>
2023-10-10 10:02:06 -0700

    pre addDataset merge version

---
+++ src/controller/Result.ts
@@ -0,0 +1,12 @@
+import {
+    IInsightFacade,
+    InsightDataset,
+    InsightDatasetKind,
+    InsightError,
+    InsightResult,
+    NotFoundError,
+} from "./IInsightFacade";
+
+export class InsightResult implements InsightResult{
+
+}
